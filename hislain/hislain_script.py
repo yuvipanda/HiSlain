a8fa64be30158ba6615a32b0a3d9bd838fd4e49e
Yuvi Panda <yuvipanda@gmail.com>
2009-06-12 21:52:10 +0530

    Added rudimentary untested RSS Publishing

--- hislain/hislain_script.py
+++ hislain/hislain_script.py
@@ -5,6 +5,8 @@ import shutil
 from dateutil import parser
 from datetime import datetime
 
+import PyRSS2Gen as RSS2
+
 import core
 import utils
 
@@ -35,13 +37,35 @@ def publish(blog):
         publish_post(p, blog.env.get_template('post.html'), blog.settings)
         print "Published Post %s" % p.title
 
-    #Publish RSS
 
     #Publish Home Page
     home_posts = sorted(blog.posts, key=lambda p: p.meta['published'])[:5]
     publish_posts(home_posts, blog.env.get_template('posts.html'), blog.settings, output_path='home', title='Home Page', permalink='/')
     print "Published Home page"
   
+    # Publish RSS
+    rss_posts = [ 
+            RSS2.RSSItem(
+                title=p.title,
+                link=blog.settings['base_url'] + p.meta['permalink'],
+                description=p.content,
+                guid=RSS2.Guid(blog.settings['base_url'] + p.meta['permalink']),
+                pubDate=p.meta['published']
+                )
+            for p in home_posts
+            ]
+
+    rss = RSS2.RSS2(
+            title=blog.settings['name'],
+            link=blog.settings['base_url'],
+            lastBuildDate=datetime.now(),
+            description=blog.settings['tagline'],
+            items=rss_posts )
+    rss_file = file(os.path.join(blog.settings['out_path'],'feed'),'w')
+    rss.write_xml(rss_file)
+    rss_file.close()
+    print "Published RSS Feed"
+
     # Publish Pages
     for p in blog.pages:
         publish_post(p, blog.env.get_template('post.html'), blog.settings)
