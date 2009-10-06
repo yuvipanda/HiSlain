080834e32a56a95c81fce0380a418e66db36469a
Yuvi Panda <yuvipanda@gmail.com>
2009-10-06 22:27:51 +0530

    Added setuptools deployment

---
+++ setup.py
@@ -0,0 +1,13 @@
+from ez_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup
+setup(name='hislain',
+      description='Static Blog Publishing System',
+      author='YuviPanda',
+      author_email='me@yuvi.in',
+      version='0.1',
+      packages=['hislain'],
+      scripts=['hislain/hislain'],
+      install_requires=['PyYAML', 'Jinja2', 'simplejson', 'PyRSS2Gen', 'Markdown', 'python-dateutil']
+      )
